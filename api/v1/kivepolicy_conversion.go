5ab0c9051d44753df4f5db32bc212f632e6d444d
Mario Kahlhofer <mario.kahlhofer@dynatrace.com>
2025-10-31 19:46:20 +0100

    refactor(kivedata, kivepolicy): refine conversion functions

--- api/v1/kivepolicy_conversion.go
+++ api/v1/kivepolicy_conversion.go
@@ -13,96 +13,86 @@
 package v1
 
 import (
+	"maps"
+
 	v2alpha1 "github.com/San7o/kivebpf/api/v2alpha1"
 	"sigs.k8s.io/controller-runtime/pkg/conversion"
 )
 
-// From v1 to v2alpha1
+// ConvertTo converts this KivePolicy (v1) to the Hub version (v2alpha1)
 func (src *KivePolicy) ConvertTo(dstRaw conversion.Hub) error {
-
 	dst := dstRaw.(*v2alpha1.KivePolicy)
 
 	dst.ObjectMeta = *src.ObjectMeta.DeepCopy()
 	dst.Spec.AlertVersion = src.Spec.AlertVersion
 
-	trapsv2 := []v2alpha1.KiveTrap{}
-	for _, trap := range src.Spec.Traps {
+	traps_v2 := []v2alpha1.KiveTrap{}
+	for _, trap_v1 := range src.Spec.Traps {
+		trap_v2 := v2alpha1.KiveTrap{}
+		trap_v2.Path = trap_v1.Path
+		trap_v2.Create = trap_v1.Create
+		trap_v2.Mode = trap_v1.Mode
+		trap_v2.Callback = trap_v1.Callback
 
-		trapv2 := v2alpha1.KiveTrap{}
-		trapv2.Path = trap.Path
-		trapv2.Create = trap.Create
-		trapv2.Mode = trap.Mode
-		trapv2.Callback = trap.Callback
-		trapv2.Metadata = map[string]string{}
+		trap_v2.Metadata = map[string]string{}
+		maps.Copy(trap_v2.Metadata, trap_v1.Metadata)
 
-		for key, value := range trap.Metadata {
-			trapv2.Metadata[key] = value
+		matchany_v2 := []v2alpha1.KiveTrapMatch{}
+		for _, match_v1 := range trap_v1.MatchAny {
+			match_v2 := v2alpha1.KiveTrapMatch{}
+			match_v2.PodName = match_v1.PodName
+			match_v2.ContainerName = match_v1.ContainerName
+			match_v2.Namespace = match_v1.Namespace
+			match_v2.IP = match_v1.IP
+			match_v2.MatchLabels = map[string]string{}
+			maps.Copy(match_v2.MatchLabels, match_v1.MatchLabels)
+
+			matchany_v2 = append(matchany_v2, match_v2)
 		}
 
-		matchesv2 := []v2alpha1.KiveTrapMatch{}
-
-		for _, match := range trap.MatchAny {
-
-			matchv2 := v2alpha1.KiveTrapMatch{}
-			matchv2.PodName = match.PodName
-			matchv2.ContainerName = match.ContainerName
-			matchv2.Namespace = match.Namespace
-			matchv2.IP = match.IP
-			matchv2.MatchLabels = match.MatchLabels
-
-			matchesv2 = append(matchesv2, matchv2)
-		}
-
-		trapv2.MatchAny = matchesv2
-		trapsv2 = append(trapsv2, trapv2)
+		trap_v2.MatchAny = matchany_v2
+		traps_v2 = append(traps_v2, trap_v2)
 	}
 
-	dst.Spec.Traps = trapsv2
-
+	dst.Spec.Traps = traps_v2
 	return nil
 }
 
-// From v2alpha1 to v1
+// ConvertFrom converts the Hub version (v2alpha1) to this KivePolicy (v1)
 func (dst *KivePolicy) ConvertFrom(srcRaw conversion.Hub) error {
-
 	src := srcRaw.(*v2alpha1.KivePolicy)
 
 	dst.ObjectMeta = *src.ObjectMeta.DeepCopy()
 	dst.Spec.AlertVersion = src.Spec.AlertVersion
 
-	trapsv1 := []KiveTrap{}
-	for _, trap := range src.Spec.Traps {
+	traps_v1 := []KiveTrap{}
+	for _, trap_v2 := range src.Spec.Traps {
+		trap_v1 := KiveTrap{}
+		trap_v1.Path = trap_v2.Path
+		trap_v1.Create = trap_v2.Create
+		trap_v1.Mode = trap_v2.Mode
+		trap_v1.Callback = trap_v2.Callback
 
-		trapv1 := KiveTrap{}
-		trapv1.Path = trap.Path
-		trapv1.Create = trap.Create
-		trapv1.Mode = trap.Mode
-		trapv1.Callback = trap.Callback
-		trapv1.Metadata = map[string]string{}
+		trap_v1.Metadata = map[string]string{}
+		maps.Copy(trap_v1.Metadata, trap_v2.Metadata)
 
-		for key, value := range trap.Metadata {
-			trapv1.Metadata[key] = value
+		matchany_v1 := []KiveTrapMatch{}
+		for _, match_v2 := range trap_v2.MatchAny {
+			match_v1 := KiveTrapMatch{}
+			match_v1.PodName = match_v2.PodName
+			match_v1.ContainerName = match_v2.ContainerName
+			match_v1.Namespace = match_v2.Namespace
+			match_v1.IP = match_v2.IP
+			match_v1.MatchLabels = map[string]string{}
+			maps.Copy(match_v1.MatchLabels, match_v2.MatchLabels)
+
+			matchany_v1 = append(matchany_v1, match_v1)
 		}
 
-		matchesv1 := []KiveTrapMatch{}
-
-		for _, match := range trap.MatchAny {
-
-			matchv1 := KiveTrapMatch{}
-			matchv1.PodName = match.PodName
-			matchv1.ContainerName = match.ContainerName
-			matchv1.Namespace = match.Namespace
-			matchv1.IP = match.IP
-			matchv1.MatchLabels = match.MatchLabels
-
-			matchesv1 = append(matchesv1, matchv1)
-		}
-
-		trapv1.MatchAny = matchesv1
-		trapsv1 = append(trapsv1, trapv1)
+		trap_v1.MatchAny = matchany_v1
+		traps_v1 = append(traps_v1, trap_v1)
 	}
 
-	dst.Spec.Traps = trapsv1
-
+	dst.Spec.Traps = traps_v1
 	return nil
 }
